f8fd450d36f7cf947124ed0c5e28d00027973e2d
Gašper <gaspersoos12@gmail.com>
2026-05-07 18:45:02 +0200

    popravek poti za uvoz Hero komponente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Hero from "../components/Hero";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main>
       <Hero />
+      {/* Kasneje bova tukaj dodala še ostale sekcije */}
     </main>
-  );
+  )
 }
